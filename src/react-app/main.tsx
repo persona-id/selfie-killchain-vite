891624cd6b344a162a9905132af0e3ce4aa70678
Gabe Young <gabe@withpersona.com>
2026-07-30 13:53:50 -0700

    fix: remove private Persona design dependencies

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,17 +1,10 @@
-import '@persona-id/design-tokens/css/variables.css'
-import '@persona-id/design-tokens/css/colors.css'
-import theme from '@persona-id/design-system/theme'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  {/* @ts-expect-error - styled-components v5 types have minor incompatibility with React types */}
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 )
